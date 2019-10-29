1e0dccc9d1122004181fd2538994d5e7434b663b
Student <example@example.com>
2019-10-29 13:08:29 -0700

    add for loop to greet each friend

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function(event){
+
+  var friends = ["Hilary", "Amy", "Bianca"];
+  for (var i = 0; i < friends.length; i++){
+    if (friends[i] < friends.length)
+    friends[i]
+    alert("Hi," + friends[i]);
+  }
+
+
+
+
+
+
+
+
+});
